5c97ab7735600ae41d33a14936577bf24021dfb5
ValentinaDimitr <>
2021-07-01 12:28:56 +0000

    demos

--- demos/Week2/menu.py
+++ demos/Week2/menu.py
@@ -1,21 +1,28 @@
-print("Please choose an option:\n\n1 - Nice message\n2 - Area of a rectangle\n3 - Area of a triangle\n4 - Multiplication table")
+while True:
+  print("Please choose an option:\n\n1 - Nice message\n2 - Area of a rectangle\n3 - Area of a triangle\n4 - Multiplication table\n0- Exit the application")
+  option = int(input("\n Enter option:"))
 
-option = int(input("\n Enter option:"))
+  if option ==1:
+    print("You don't smell so bad today!")
+  elif option == 2:
+    l = float(input("Enter the lenght of the rectangle:"))
 
-if option ==1:
-  print("You don't smell so bad today!")
-elif option == 2:
-  l = float(input("Enter the lenght of the rectangle:"))
+    w = float(input("Enter the height of the rectangle:"))
+    area = l*w
+    print("Area of this rectangle is {:.2f}".format(l*w))
+  elif option ==3:
+    b = float(input("Enter the base of the triangle:"))
 
-  w = float(input("Enter the height of the rectangle:"))
-  area = l*w
-  print("Area of this rectangle is {:.2f}".format(l*w))
-elif option ==3:
-  b = float(input("Enter the base of the triangle:"))
-
-  h = float(input("Enter the height of the triangle:"))
-  print("Area of this triangle is {:.2f}".format(b*h/2))
-elif option == 4:
-  pass
-else:
-  print("Go to Specsavers")
+    h = float(input("Enter the height of the triangle:"))
+    print("Area of this triangle is {:.2f}".format(b*h/2))
+  elif option == 4:
+    n = int(input("Enter the number:"))
+    counter = 1
+    n2 = int(input("Enter the limit of the table:"))
+    while counter <= n2:
+      print(f"{n}x{counter} = {counter*n}")
+      counter +=1
+  elif option == 0:
+      break
+  else:
+    print("Go to Specsavers")
